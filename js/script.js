0f0d0a1b044173ab203bacd4e7224933fe239706
Kris Wignes <kriswignes@gmail.com>
2018-01-22 20:17:07 -0800

    layout update

--- js/script.js
+++ js/script.js
@@ -1,35 +1,71 @@
-// external js: isotope.pkgd.js
+$(document).ready( function() {
 
-// init Isotope
-var $grid = $('.grid').isotope({
-  itemSelector: '.element-item',
-  layoutMode: 'fitRows'
+var $grid = $('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  gutter: 20,
+  percentPosition: true
 });
-// filter functions
-var filterFns = {
-  // show if number is greater than 50
-  numberGreaterThan50: function() {
-    var number = $(this).find('.number').text();
-    return parseInt( number, 10 ) > 50;
-  },
-  // show if name ends with -ium
-  ium: function() {
-    var name = $(this).find('.name').text();
-    return name.match( /ium$/ );
-  }
-};
-// bind filter button click
-$('.filters-button-group').on( 'click', 'button', function() {
-  var filterValue = $( this ).attr('data-filter');
-  // use filterFn if matches value
-  filterValue = filterFns[ filterValue ] || filterValue;
-  $grid.isotope({ filter: filterValue });
+
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry('layout');
 });
-// change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
+
+});
+
+
+    // ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 550) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(250);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(250);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 300);
+});
+
+
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a.down").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 300, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
   });
 });
+
+
+
+var nav = responsiveNav(".nav-collapse");
+
+  $(document).on("scroll", function(){
+    if
+      ($(document).scrollTop() > 100){
+      $("header").addClass("shrink");
+    }
+    else
+    {
+      $("header").removeClass("shrink");
+    }
+  });
