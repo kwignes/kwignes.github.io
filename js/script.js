c0e977337088ac5557bc946507ee5913a681d3fe
Kris Wignes <kriswignes@gmail.com>
2018-05-22 00:17:14 -0700

    layout update

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready( function() {
 var $grid = $('.grid').masonry({
   itemSelector: '.grid-item',
   columnWidth: '.grid-sizer',
-  gutter: 60,
+  gutter: 40,
   percentPosition: true
 });
 
@@ -30,42 +30,35 @@ $('#return-to-top').click(function() {      // When arrow is clicked
 
 
 
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a.down, .intro a, nav li:last-child a").on('click', function(event) {
+// $(document).ready(function(){
+//   $("a.down, .intro a, nav li:last-child a").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+//     if (this.hash !== "") {
+//       event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+//       var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
+//       $('html, body').animate({
+//         scrollTop: $(hash).offset().top
+//       }, 800, function(){
    
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
+//         window.location.hash = hash;
+//       });
+//     } 
+//   });
+// });
 
 
 
-var nav = responsiveNav(".nav-collapse");
+// var nav = responsiveNav(".nav-collapse");
 
-  $(document).on("scroll", function(){
-    if
-      ($(document).scrollTop() > 300){
-      $("header").addClass("shrink");
-    }
-    else
-    {
-      $("header").removeClass("shrink");
-    }
-  });
+//   $(document).on("scroll", function(){
+//     if
+//       ($(document).scrollTop() > 300){
+//       $("header").addClass("shrink");
+//     }
+//     else
+//     {
+//       $("header").removeClass("shrink");
+//     }
+//   });
